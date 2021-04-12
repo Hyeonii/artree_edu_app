72fc9acdb75d8a001afc1cd75cf211bb83911a1c
Hyeonii <63392456+Hyeonii@users.noreply.github.com>
2021-04-12 23:53:41 +0900

    Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+/*  import React from "react"; 이 부분을 주석처리해도 작동하네? */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Helloooo worldddd</div>;
 }
 
 export default App;
